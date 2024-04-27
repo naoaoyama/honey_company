086c9e27efa97c4e11ebb63f273672386e4e4c06
n.aoyama <117046105+naoaoyama@users.noreply.github.com>
2024-04-27 21:04:17 +0900

    Javascript was devided

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,47 @@
-console.log('haha');
 /*ハンバーガーメニュー */
-  // function toggleMenu() {
-  //   var menu = document.getElementById("menu");
-  //   if (menu.style.display === "block") {
-  //     menu.style.display = "none";
-  //   } else {
-  //     menu.style.display = "block";
-  //   } 
-  // }
+  
+let hamburger = document.getElementById("hamburger");
+let menu = document.getElementById("menu");
 
-  let hamburger = document.getElementById("hamburger");
-  let menu = document.getElementById("menu");
-    hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
-  });
+  hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+let menu_lists = document.querySelectorAll(".menu_list");
+
+menu_lists.forEach(menu_list => {
+menu_list.addEventListener('click', () =>{
+hamburger.classList.remove("active");
+menu.classList.remove("active");
+});
+});        
+
+
+/*スライダー */
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+  loop: true, // ループ機能を有効にする
+});
+
+  
 
 /* アコーディオン */
-  
-/*スライダー */
+
+let history_buttons = document.querySelectorAll(".accordion_button");
+let history_content = document.getElementById("accordion_content");
+let accordion_button = document.getElementById("accordion_button_id");
+
+history_buttons.forEach(button => {
+button.addEventListener('click', () =>{
+history_content.classList.toggle("active");
+accordion_button.classList.toggle("aaaaa");
+});
+});    
+
+
   
